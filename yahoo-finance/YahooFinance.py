1b31e15e52e1ffee21483a6004da50d0dd1885a0
Andrew Aday <andrewladay@gmail.com>
2015-10-16 17:28:51 -0400

    replaced finance class with a generator

--- yahoo-finance/YahooFinance.py
+++ yahoo-finance/YahooFinance.py
@@ -1,57 +1,12 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from yahoo_finance import Share
 
-class Finance:
-
-	def __init__(self, ticker, startDate, endDate):
-		"""Creates wrapper instance of yahoo_finance Share class.
+def get_data(ticker, start_date, end_date):
+	"""Creates wrapper instance of yahoo_finance Share class.
 		   :param ticker: official ticker for the company
-		   :params startDate, endDate: %y-%m-%d, determines which days to get stock price, inclusive
-		"""
-		self.ticker = ticker
-		self._share = Share(self.ticker)
-		self._historicalVerbose = self._share.get_historical(startDate,endDate)
-		self._historical = self._processData("both")
-		self._close = self._processData("close")
-		self._open = self._processData("open")
-		self._iterator = Stack(self._historical) 
+		   :params startDate, endDate: Datetime.date objects, determines which days to get stock price, inclusive
+	"""
+	shares = Share(ticker)
+	prices = shares.get_historical(start_date.isoformat(),end_date.isoformat())
 
-	def _processData(self,includes="both"):
-		""" Parse the open and close prices from _historicalVerbose """
-		if includes == "both":
-			return [(day['Open'],day['Close']) for day in self._historicalVerbose]
-		if includes == "close"
-			return [day['Close'] for day in self._historicalVerbose]
-		if includes == "open":
-			return [day['Open'] for day in self._historicalVerbose]
-
-	def microBatch(self):
-		""" Return 1 Day of data """
-		return self._iterator.pop() if not self._iterator.isEmpty() else 'EMPTY'
-
-	def macroBatch(self, includes="both"):
-		""" Returns list of all open/close stock prices from startDate to endDate """
-		if includes == "both":
-			return self._historical
-		if includes == "close":
-			return self._close
-		if includes == "open": 
-			return self._open
-
-	def getRaw(self):
-		"""Return raw output from Yahoo Finance API"""
-		return self._historicalVerbose
-
-class Stack:
-	def __init__(self, data):
-		self.__data = data
-
-	def isEmpty(self):
-		return len(self.__data) == 0
-
-	def pop(self):
-		return self.__data.pop()
-
-
-
+	for day in prices:
+		yield day["Open"], day["Close"]
