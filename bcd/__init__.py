cacbcbdc51267ee3a899050af1ba26b1515ba359
Alan Du <alanhdu@gmail.com>
2015-10-23 18:08:19 -0400

    Docstring big bang

--- bcd/__init__.py
+++ bcd/__init__.py
@@ -1,22 +1,46 @@
+"""Local implementation for Bayesian Changepoint Detection"""
+
 import numpy as np
 
 
 class BayesianChangepointDetection(object):
+    """ Bayesian Changepoint Detection implementation
+
+    Based on algorithm from http://arxiv.org/abs/0710.3742.
+
+    Args:
+        hazard_function: A function that maps from the length of the current
+                         run to the probability of a changepoint.
+                         Callable[[int], float]
+        distribution: The distribution that the time series is assumed to
+                      follow. Should follow interface defined in
+                      `bcd.distributions.Distribution`.
+    """
     def __init__(self, hazard, distribution):
         self.hazard = hazard
         self.distribution = distribution
         self.time = 0
         self.Pr = np.ones(1)
 
-    def step(self, x):
+    def step(self, observation):
+        """ Calculates the probability of a changepoint at the current time
+
+        Args:
+            data: The latest observation. The type can be anything that
+                  the distribution's pdf accepts.
+        Returns:
+            A 1-D np.array of floats representing the probability
+            distribution of the current run length. Output[r_0] is the
+            probability that r=r_0 at the current time.
+        """
         self.time += 1
 
         H = self.hazard(np.arange(self.time))
         old = self.Pr
         self.Pr = np.zeros(self.time + 1)   # +1 because 0 <= r <= time
 
-        predprob = self.distribution.pdf(x)
-        self.distribution.update(x)
+        predprob = self.distribution.pdf(observation)
+        self.distribution.update(observation)
 
         self.Pr[1:] = old * predprob * (1 - H)
         self.Pr[0] = np.sum(old * predprob * H)
@@ -26,6 +50,25 @@ class BayesianChangepointDetection(object):
 
 
 def offline_changepoint_detection(data, hazard_function, distribution):
+    """ Reference changepoint detection implementation
+
+    Assumes that length of data is known beforehand. Implementation from
+    https://github.com/hildensia/bayesian_changepoint_detection.
+
+    Args:
+        data: A list of floats that represents the data to analyze.
+              data[t] should be the observed data @ time t.
+        hazard_function: A function that returns the (prior) probability
+                         of a changepoint given the current run length.
+                         Callable[[int], float]
+        distribution: The distribution that the time series is assumed
+                      to follow. Should follow interface defined in
+                      `bcd.distributions.Distribution`.
+    Returns:
+        A two-dimensional numpy array Pr, where Pr[r_0, t] is the
+        probability that r = r_0 @ time t.
+    """
+
     length = len(data)
     H = hazard_function(np.arange(length + 1))
     Pr = np.zeros((length + 1, length + 1))    # length + 1 b/c 0 is for priors
--- bcd/distributions.py
+++ bcd/distributions.py
@@ -1,68 +1,153 @@
+""" Defines prior distributions for bayesian changepoint detection
+"""
+
 from scipy import stats
 import numpy as np
 
 
-class Gaussian(object):
-    """ Adapted from http://engineering.richrelevance.com/
-    bayesian-analysis-of-normal-distributions-with-python/,
-    http://www.cs.ubc.ca/~murphyk/Papers/bayesGauss.pdf
+class Distribution(object):
+    """Base class for probability distributions used in BCD.
 
-        Parameters stored in array
+    Note that each instance represents _multiple_ different pdfs, each
+    assuming a different run length. So, after calling `update` t times,
+    there are (t + 1) different possible values of r (0 <= r <= t).
+    `pdf` should thus return an array of length (t + 1).
+
+    We thus store (t + 1) copies of each hyperparameter.
+    hyperparameter[0] is our prior distribution, hyperparameter[1] is
+    the distribution fitted on 1 observation, hyperparameter[2] is the
+    distribution fitted on 2 observations, and so on.
+    """
+
+    def pdf(self, data):
+        """ Get the prob density of the observation conditioned on run length.
+
+        Note that calling pdf _does not_ automatically call update. You
+        must do this manually.
+
+        Args:
+            data: Latest observtion
+        Returns:
+            A 1-D array of floats, where pdf[t] is the probability
+            density of data assuming that the run length is t and given
+            our fitted hyperparameters.
+            pdf[t] = p(data | r=t, hyperparameters)
+        """
+        raise NotImplementedError
+
+    def update(self, data):
+        """ Update the hyperparameters given the current observation
+        Args:
+            data: The latest observation
+        Returns:
+            None
+        """
+
+        raise NotImplementedError
+
+
+class Gaussian(Distribution):
+    """ Gaussian Distribution
+
+    Adapted from http://www.cs.ubc.ca/~murphyk/Papers/bayesGauss.pdf
+    and http://goo.gl/gfyils.
+
+    Args:
+        kappa: The prior for kappa
+        mu:    The prior for mu
+        alpha: The prior for alpha
+        beta:  The prior for beta
     """
 
     def __init__(self, kappa, mu, alpha, beta):
-
+        """ Initialize the prior distribution """
         self.kappa0 = self.kappa = np.array([kappa])  # Certainty
-        self.mu0 = self.mu = np.array([mu])  # Mean
+        self.mu0 = self.mu = np.array([mu])           # Mean
         self.alpha0 = self.alpha = np.array([alpha])  # Gamma
-        self.beta0 = self.beta = np.array([beta])  # Sum of Squares
+        self.beta0 = self.beta = np.array([beta])     # Sum of Squares
 
     def pdf(self, data):
-        return stats.norm.pdf(x=data,
-                              loc=self.mu,
-                              scale=np.sqrt(self.beta / self.kappa))
+        """ Calculate probability density at data at all possible run lengths.
+
+        This does _not_ automatically call update.
+
+        Args:
+            data: float that is the latest observtion
+        Returns:
+            A 1-D array of floats, whose length is equal to the number
+            of times update is called
+        """
+        scale = np.sqrt(self.beta / self.kappa)
+        return stats.norm.pdf(x=data, loc=self.mu, scale=scale)
 
     def update(self, data):
+        """ Update the hyperparameters
+
+        Args:
+            data: float that is the latest observation
+        Returns:
+            None
+        """
+
         new_kappa = self.kappa + 1
         new_mu = (self.kappa * self.mu + data) / (self.kappa + 1)
         new_alpha = (self.alpha + 0.5)
         new_beta = self.beta + ((self.kappa * (data - self.mu) ** 2) /
                                 (2 * self.kappa + 1))
 
-        self.kappa = np.concatenate(self.kappa, new_kappa)
-        self.mu = np.concatenate(self.mu, new_mu)
-        self.alpha = np.concatenate(self.alpha, new_alpha)
-        self.beta = np.concatenate(self.beta, new_beta)
+        self.kappa = np.concatenate([self.kappa0, new_kappa])
+        self.mu = np.concatenate([self.mu0, new_mu])
+        self.alpha = np.concatenate([self.alpha0, new_alpha])
+        self.beta = np.concatenate([self.beta0, new_beta])
 
 
-class StudentT(object):
-    """ Taken from https://github.com/hildensia/bayesian_changepoint_detection
+class StudentT(Distribution):
+    """ Student's T distribution prior.
 
-        Parameters stored in array
+    Adapted from https://github.com/hildensia/bayesian_changepoint_detection
+
+    Args:
+        kappa: The prior for kappa
+        mu:    The prior for mu
+        alpha: The prior for alpha
+        beta:  The prior for beta
     """
     def __init__(self, kappa, mu, alpha, beta):
         self.kappa0 = self.kappa = np.array([kappa])  # Certainty
-        self.mu0 = self.mu = np.array([mu])  # Mean
+        self.mu0 = self.mu = np.array([mu])           # Mean
         self.alpha0 = self.alpha = np.array([alpha])  # Gamma
-        self.beta0 = self.beta = np.array([beta])  # Sum of Squares
+        self.beta0 = self.beta = np.array([beta])     # Sum of Squares
 
     def pdf(self, data):
-        return stats.t.pdf(x=data,
-                           df=2*self.alpha,
-                           loc=self.mu,
-                           scale=np.sqrt(self.beta * (self.kappa+1) /
-                                         (self.alpha * self.kappa)))
+        """ Calculate probability density at data at all possible run lengths.
+
+        This does _not_ automatically call update.
+
+        Args:
+            data: float that is the latest observtion
+        Returns:
+            A 1-D array of floats, whose length is equal to the number
+            of times update is called
+        """
+        scale = np.sqrt(self.beta * (self.kappa + 1) /
+                        (self.alpha * self.kappa))
+        return stats.t.pdf(x=data, df=2*self.alpha, loc=self.mu, scale=scale)
 
     def update(self, data):
-        kappa_update = np.concatenate((self.kappa0, self.kappa + 1.))
-        mu_update = np.concatenate((self.mu0, (self.kappa *
-                                   self.mu + data) / (self.kappa + 1)))
-        alpha_update = np.concatenate((self.alpha0, self.alpha + 0.5))
-        beta_update = np.concatenate((self.beta0, self.beta + (self.kappa *
-                                     (data - self.mu)**2) /
-                                    (2. * (self.kappa + 1.))))
+        """ Update the hyperparameters given the current observation
 
-        self.kappa = kappa_update
-        self.mu = mu_update
-        self.alpha = alpha_update
-        self.beta = beta_update
+        Args:
+            data: float that is the latest observation
+        Returns:
+            None
+        """
+        new_kappa = self.kappa + 1
+        new_mu = (self.kappa * self.mu + data) / (self.kappa + 1)
+        new_alpha = self.alpha + 0.5
+        new_beta = self.beta + (self.kappa * (data - self.mu ** 2) /
+                                (2 * self.kappa + 2))
+
+        self.kappa = np.concatenate([self.kappa0, new_kappa])
+        self.mu = np.concatenate([self.mu0, new_mu])
+        self.alpha = np.concatenate([self.alpha0, new_alpha])
+        self.beta = np.concatenate([self.beta0, new_beta])
