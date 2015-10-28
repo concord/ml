9847c7fea2043ac93a0dfd3d593faed68e2b7df7
Alan Du <alanhdu@gmail.com>
2015-10-28 16:33:06 -0400

    Make get_stock_prices return a pandas DataFrame

--- data/yahoo.py
+++ data/yahoo.py
@@ -1,4 +1,6 @@
 import datetime as dt
+
+import pandas as pd
 from yahoo_finance import Share
 
 
@@ -11,15 +13,16 @@ def get_stock_prices(ticker, start_date, end_date=None):
         end_date (datetime.date): date to end collecting share prices. Default
             to today.
     Returns:
-        An iterable of (open, close) price tuples.
-        Iterable[(float, float)]
+        A pd.DataFrame with the Date as the index and with columns
+        ["Open", "Close", "High", "Low", "Volume", "Adj_Close"]
     """
     if end_date is None:
         end_date = dt.date.today()
 
     shares = Share(ticker)
-    prices = shares.get_historical(start_date.isoformat(),
-                                   end_date.isoformat())
-
-    for day in prices:
-        yield float(day["Open"]), float(day["Close"])
+    df = pd.DataFrame(shares.get_historical(start_date.isoformat(),
+                                            end_date.isoformat()))
+    return df.set_index("Date", drop=True) \
+             .drop("Symbol", axis=1) \
+             .astype(float) \
+             .sort_index()
