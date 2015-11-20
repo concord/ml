eedf92339ea909cdbc37b4f9824e87d4dbc4c480
Alan Du <alanhdu@gmail.com>
2015-11-20 13:31:09 -0500

    Slightly refactor logic

--- bcd/distributions.py
+++ bcd/distributions.py
@@ -96,11 +96,12 @@ class Gaussian(Distribution):
             None
         """
 
+        delta = observation - self.mu
+
         new_kappa = self.kappa + 1
-        new_mu = (self.kappa * self.mu + observation) / (self.kappa + 1)
-        new_alpha = (self.alpha + 0.5)
-        new_beta = self.beta + ((self.kappa * (observation - self.mu) ** 2) /
-                                (2 * self.kappa + 2))
+        new_mu = self.mu + delta / new_kappa
+        new_alpha = self.alpha + 0.5
+        new_beta = self.beta + (self.kappa * delta ** 2 / (2 * self.kappa + 2))
 
         self.kappa = np.concatenate([self.kappa0, new_kappa])
         self.mu = np.concatenate([self.mu0, new_mu])
