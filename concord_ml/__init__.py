1e398c96498de93df9c33332eee2fa1a9c55ca9c
Alan Du <alanhdu@gmail.com>
2016-05-27 19:59:46 -0400

    Remove data functions
    Those are really orthogonal to the rest of the package

--- concord_ml/__init__.py
+++ concord_ml/__init__.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
 
 from . import bcd       # noqa
-from . import data      # noqa
 from .sklearn import SklearnPredict, SklearnTransform   # noqa
--- concord_ml/data/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-from .yahoo import get_stock_prices
-from .computations import Generator
-
-__all__ = [get_stock_prices, Generator]
